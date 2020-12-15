e6dcf4509fbbff9e0d01b3087420bbebc6810013
Julia <sascaui@yahoo.com>
2020-12-15 07:57:56 +0000

    no more tree

---
+++ project/Assets/IS_Scripts/Game scripts/IS_Last_Checkpoint.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IS_Last_Checkpoint : MonoBehaviour
+{
+    public Material Mat1;
+    public Material Blue;
+    public Transform NextCheckpoint;
+
+    private Renderer _Renderer;
+    private IS_LevelManager levelManagerReference;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      _Renderer = gameObject.GetComponent<Renderer>();
+      _Renderer.material = Blue;
+      levelManagerReference = GameObject.Find("LevelManager").GetComponent<IS_LevelManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+{
+    //print (_Render.material.name);
+    if(_Renderer.material.name == "Black (Instance)" && other.gameObject.tag == "Player" )
+    {
+        //print("Change");
+        _Renderer.material = Mat1;
+        levelManagerReference.lastGoodCheckpoint = NextCheckpoint.transform;
+    }
+
+}
+
+}
